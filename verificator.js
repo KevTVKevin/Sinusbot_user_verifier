143853dea7361949543490a725a79326ac0365eb
Kevin <@>
2019-10-31 14:11:59 +0100

    Added some stuff - Comments - get checked values from check Database - worker

--- verificator.js
+++ verificator.js
@@ -59,32 +59,44 @@ registerPlugin({
                 engine.log(err);
             }
     });
-
+    // Baue Tabellen auf, falls sie nicht existieren
     if (dbc) dbc.exec("CREATE TABLE IF NOT EXISTS '' + config.databasePrefix + 'verify' ( 'id' int(11) NOT NULL AUTO_INCREMENT, 'minecraft_uuid' varchar(100) NOT NULL, 'ts_uuid' varchar(100) NOT NULL, 'verification_code' varchar(100) NOT NULL, 'verified' int(11) NOT NULL");
     if (dbc) dbc.exec("CREATE TABLE IF NOT EXISTS '' + config.databasePrefix + 'check' ( 'id' int(11) NOT NULL AUTO_INCREMENT, 'checked' int(11) NOT NULL");
 
     while (true) {
+        //Hole Anzahl der Datenbankeinträge in verify
         if (dbc) dbc.query("SELECT COUNT(*) FROM '' + config.databasePrefix + 'verify'", function (err, res) {
             if(!err) {
+                //Speichere Anzahl der Datenbankeinträge in check in der Variable count
                 var count = (dbc) ? dbc.query("SELECT COUNT(*) FROM '' + config.databasePrefix + 'check'", function (err, res){
                     if (!err) {
                         return res;
                     }
                     return -1;
                 }) : -1;
+                //Prüfe ob es mehr Einträge in verify als in check gibt
                 if(res > count) {
-                    for(let i = count+1; i > res; i++) {
-                        if (dbc) dbc.exec("INSERT INTO '' + config.databasePrefix + 'check' (0)");
+                    for(let i = count+1; i < res; i++) {
+                        //Setze die neuen Einträge in der Datenbank auf 0
+                        if (dbc) dbc.exec("INSERT INTO '' + config.databasePrefix + 'check' (checked) VALUES (0)");
                     }
                 }
             }
         });
-        if (dbc) dbc.query("SELECT COUNT(*) FROM '' + config.databasePrefix + 'check'", function (err, res) {
+        //Hole alle checked Werte aus der check Tabelle
+        var checkArray = (dbc) ? dbc.query("SELECT checked FROM '' + config.databasePrefix + 'check'", function (err, res) {
             if(!err) {
-                for(var i = 0; i < res; i++) {
-
-                }
+                return res;
             }
-        });
-    }
+            return [];
+        }) : [];
+        for(let i = 0; i < checkArray.length; i++) {
+            if(checkArray[i] === 0) {
+                var worker = new Worker("checkUser.js");
+                worker.addEventListener("message", function (event) {
+
+                });
+            }
+        }
+     }
 });
